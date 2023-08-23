ff9d7f3500d97e0b5ab5b65d95b40e0a83187c1c
CristianmMst <cristianmmst@gmail.com>
2023-08-23 13:54:02 -0500

    remove proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,14 +5,6 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-    },
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
