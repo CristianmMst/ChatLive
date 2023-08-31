4fa1f76de6b74188664b87fe805e9dd8e9f1b1e5
CristianmMst <cristianmmst@gmail.com>
2023-08-30 19:42:59 -0500

    rename CLIENT_URL

--- client/src/consts/index.ts
+++ client/src/consts/index.ts
@@ -1,2 +1,2 @@
-export const CLIENT_URL = "https://chat-live-dun.vercel.app";
+export const CLIENT_URL = "https://chatlive-mmst.vercel.app";
 export const API_URL = "https://chat-live-iyck.onrender.com/api";
