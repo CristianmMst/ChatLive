8427c6af3f8fca1b5598e5e4b65c344d89f307cb
CristianmMst <cristianmmst@gmail.com>
2023-11-30 16:28:41 -0500

    add urls prod & dev

--- server/src/const/index.ts
+++ server/src/const/index.ts
@@ -1,2 +1,12 @@
-export const CLIENT_URL = "https://chatlive-mmst.vercel.app";
-export const API_URL = "https://chat-live-iyck.onrender.com/api";
+enum URLS_CLIENT {
+  DEV = "http://localhost:5173",
+  PROD = "https://chatlive-mmst.vercel.app",
+}
+
+enum URLS_SERVER {
+  DEV = "http://localhost:3000/api",
+  PROD = "https://chat-live-iyck.onrender.com/api",
+}
+
+export const API_URL = URLS_SERVER.DEV;
+export const CLIENT_URL = URLS_CLIENT.DEV;
