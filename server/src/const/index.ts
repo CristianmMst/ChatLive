89f163aabbec75009c4bb62951e65bdbf95229b1
CristianmMst <cristianmmst@gmail.com>
2024-07-30 23:25:38 -0500

    add envs

--- server/src/const/index.ts
+++ server/src/const/index.ts
@@ -1,12 +1,2 @@
-enum URLS_CLIENT {
-  DEV = "http://localhost:5173",
-  PROD = "https://chatlive-mmst.vercel.app",
-}
-
-enum URLS_SERVER {
-  DEV = "http://localhost:3000/api",
-  PROD = "https://chatlive-xat8.onrender.com/api",
-}
-
-export const API_URL = URLS_SERVER.PROD;
-export const CLIENT_URL = URLS_CLIENT.PROD;
+export const CLIENT_URL = process.env.URL_CLIENT || "http://localhost:5173";
+export const API_URL = process.env.URL_SERVER || "http://localhost:3000/api";
