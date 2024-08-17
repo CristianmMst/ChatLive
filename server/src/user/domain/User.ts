09c29981caf98f1d56bfe49401e3bc52b7787177
CristianmMst <cristianmmst@gmail.com>
2024-08-16 23:56:25 -0500

    change entity User

--- server/src/user/domain/User.ts
+++ server/src/user/domain/User.ts
@@ -1,18 +1,21 @@
-export interface Contacts {
-  id: string;
-  username: string;
-}
-
 export class User {
   readonly id: string | undefined;
   readonly email: string;
   readonly username: string;
-  readonly password: string;
+  readonly password: string | null;
+  readonly googleId: string | null;
 
-  constructor(email: string, username: string, password: string, id?: string) {
+  constructor(
+    email: string,
+    username: string,
+    password: string | null = null,
+    googleId: string | null = null,
+    id?: string,
+  ) {
     this.id = id;
     this.email = email;
     this.username = username;
     this.password = password;
+    this.googleId = googleId;
   }
 }
--- server/src/user/infrastructure/models/userModel.ts
+++ server/src/user/infrastructure/models/userModel.ts
@@ -1,16 +1,21 @@
 import mongoose, { Document, Schema } from "mongoose";
 
 export interface UserDocument extends Document {
-  id: string;
+  id: string | undefined;
   email: string;
   username: string;
-  password: string;
+  password: string | null;
+  googleId: string | null;
 }
 
-const UserSchema: Schema = new Schema({
-  email: { type: String, required: true, unique: true },
-  username: { type: String, required: true },
-  password: { type: String, required: true },
-});
+const UserSchema: Schema = new Schema(
+  {
+    email: { type: String, required: true, unique: true, sparse: true },
+    googleId: { type: String, required: false, unique: true, sparse: true },
+    username: { type: String, required: true },
+    password: { type: String, required: false },
+  },
+  { versionKey: false },
+);
 
 export const userModel = mongoose.model<UserDocument>("User", UserSchema);
