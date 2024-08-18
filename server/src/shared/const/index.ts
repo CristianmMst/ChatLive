fb88f8a3ce07b15dc78eb52af0f2ad84289a31fd
CristianmMst <cristianmmst@gmail.com>
2024-08-18 02:18:33 -0500

    fix url redirect

--- server/src/shared/const/index.ts
+++ server/src/shared/const/index.ts
@@ -1,2 +1,2 @@
-export const CLIENT_URL = process.env.URL_CLIENT || "http://localhost:5173";
-export const API_URL = process.env.URL_SERVER || "http://localhost:3000/api";
+export const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
+export const API_URL = process.env.API_URL || "http://localhost:3000/api";
