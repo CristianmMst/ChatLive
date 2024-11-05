d6c0bb87603c14b565fbec44382b3e373af3dd7d
CristianmMst <cristianmmst@gmail.com>
2024-11-05 14:24:12 -0500

    fix client_url server

--- apps/server/src/shared/const/index.ts
+++ apps/server/src/shared/const/index.ts
@@ -1,2 +1,2 @@
-export const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
+export const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:3000";
 export const API_URL = process.env.API_URL || "http://localhost:3000/api";
